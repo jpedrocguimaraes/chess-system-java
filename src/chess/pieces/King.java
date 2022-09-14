0353acc8b6a4aaa9d8e4a8e470d024c1c20021c8
João Pedro <jpedrocguimaraes@hotmail.com>
2022-09-14 15:39:31 -0300

    Implementado movimentos do Rei

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -14,10 +15,65 @@ public class King extends ChessPiece {
 	public String toString() {
 		return "K";
 	}
+	
+	private boolean canMove(Position position) {
+		ChessPiece piece = (ChessPiece) getBoard().piece(position);
+		return piece == null || piece.getColor() != getColor();
+	}
 
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] moves = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		Position position = new Position(0, 0);
+		
+		//cima
+		position.setValues(this.position.getRow() - 1, this.position.getColumn());
+		if(getBoard().positionExists(position) && canMove(position)) {
+			moves[position.getRow()][position.getColumn()] = true;
+		}
+		
+		//noroeste
+		position.setValues(this.position.getRow() - 1, this.position.getColumn() - 1);
+		if(getBoard().positionExists(position) && canMove(position)) {
+			moves[position.getRow()][position.getColumn()] = true;
+		}
+		
+		//nordeste
+		position.setValues(this.position.getRow() - 1, this.position.getColumn() + 1);
+		if(getBoard().positionExists(position) && canMove(position)) {
+			moves[position.getRow()][position.getColumn()] = true;
+		}
+		
+		//esquerda
+		position.setValues(this.position.getRow(), this.position.getColumn() - 1);
+		if(getBoard().positionExists(position) && canMove(position)) {
+			moves[position.getRow()][position.getColumn()] = true;
+		}
+		
+		//direita
+		position.setValues(this.position.getRow(), this.position.getColumn() + 1);
+		if(getBoard().positionExists(position) && canMove(position)) {
+			moves[position.getRow()][position.getColumn()] = true;
+		}
+		
+		//sudoeste
+		position.setValues(this.position.getRow() + 1, this.position.getColumn() - 1);
+		if(getBoard().positionExists(position) && canMove(position)) {
+			moves[position.getRow()][position.getColumn()] = true;
+		}
+		
+		//sudeste
+		position.setValues(this.position.getRow() + 1, this.position.getColumn() + 1);
+		if(getBoard().positionExists(position) && canMove(position)) {
+			moves[position.getRow()][position.getColumn()] = true;
+		}
+		
+		//baixo
+		position.setValues(this.position.getRow() + 1, this.position.getColumn());
+		if(getBoard().positionExists(position) && canMove(position)) {
+			moves[position.getRow()][position.getColumn()] = true;
+		}
+		
 		return moves;
 	}
 }
