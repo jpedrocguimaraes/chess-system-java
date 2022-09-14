6913c6c07000e9d1fb642353a415c1cec5860545
João Pedro <jpedrocguimaraes@hotmail.com>
2022-09-14 00:55:57 -0300

    Implementado movimentos da Torre

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -32,6 +32,7 @@ public class ChessMatch {
 		Position target = targetPosition.toPosition();
 		
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece) capturedPiece;
 	}
@@ -50,6 +51,12 @@ public class ChessMatch {
 		
 		if(!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("Não existem movimentos possíveis para a peça escolhida");
+		}
+	}
+	
+	private void validateTargetPosition(Position source, Position target) {
+		if(!board.piece(source).possibleMove(target)) {
+			throw new ChessException("A peça escolhida não pode se mover para a posição de destino");
 		}
 	}
 	
--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -18,6 +19,52 @@ public class Rook extends ChessPiece{
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] moves = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		Position position = new Position(0, 0);
+		
+		//cima
+		position.setValues(this.position.getRow() - 1, this.position.getColumn());
+		while(getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)) {
+			moves[position.getRow()][position.getColumn()] = true;
+			position.setRow(position.getRow() - 1);
+		}
+		
+		if(getBoard().positionExists(position) && isThereOpponentPiece(position)) {
+			moves[position.getRow()][position.getColumn()] = true;
+		}
+		
+		//esquerda
+		position.setValues(this.position.getRow(), this.position.getColumn() - 1);
+		while(getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)) {
+			moves[position.getRow()][position.getColumn()] = true;
+			position.setColumn(position.getColumn() - 1);
+		}
+		
+		if(getBoard().positionExists(position) && isThereOpponentPiece(position)) {
+			moves[position.getRow()][position.getColumn()] = true;
+		}
+		
+		//direita
+		position.setValues(this.position.getRow(), this.position.getColumn() + 1);
+		while(getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)) {
+			moves[position.getRow()][position.getColumn()] = true;
+			position.setColumn(position.getColumn() + 1);
+		}
+		
+		if(getBoard().positionExists(position) && isThereOpponentPiece(position)) {
+			moves[position.getRow()][position.getColumn()] = true;
+		}
+		
+		//baixo
+		position.setValues(this.position.getRow() + 1, this.position.getColumn());
+		while(getBoard().positionExists(position) && !getBoard().thereIsAPiece(position)) {
+			moves[position.getRow()][position.getColumn()] = true;
+			position.setRow(position.getRow() + 1);
+		}
+		
+		if(getBoard().positionExists(position) && isThereOpponentPiece(position)) {
+			moves[position.getRow()][position.getColumn()] = true;
+		}
+		
 		return moves;
 	}
 }
