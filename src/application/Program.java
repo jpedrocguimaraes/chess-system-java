f8bc4c9f172a16778b1f897025b2b9924caf982b
João Pedro <jpedrocguimaraes@hotmail.com>
2022-09-27 22:33:26 -0300

    Implementado promoção de peças

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,6 +39,12 @@ public class Program {
 				if(capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if(chessMatch.getPromoted() != null) {
+					System.out.print("Digite uma peça para promoção (R/N/B/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			} catch(ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
